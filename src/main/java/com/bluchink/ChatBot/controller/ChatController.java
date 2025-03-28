3c2c1f4c8e3a2dec7831960b0cf919a815bb1475
Pritesh2004 <pmpatilpritesh123@gmail.com>
2025-03-28 15:04:29 +0530

    correct origin

--- src/main/java/com/bluchink/ChatBot/controller/ChatController.java
+++ src/main/java/com/bluchink/ChatBot/controller/ChatController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://127.0.0.1:3000")
+@CrossOrigin(origins = "http://192.168.45.94:3000")
 public class ChatController {
 
     @Autowired
